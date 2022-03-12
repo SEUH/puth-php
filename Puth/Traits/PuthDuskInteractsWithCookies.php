bf177fc81d118717e614386126784b98cf00e70c
Sebastian Ühlin <uehlinse@gmail.com>
2022-03-12 04:11:46 +0100

    Fixed cookie assertions

--- Puth/Traits/PuthDuskInteractsWithCookies.php
+++ Puth/Traits/PuthDuskInteractsWithCookies.php
@@ -6,39 +6,39 @@ use DateTimeInterface;
 
 trait PuthDuskInteractsWithCookies
 {
-    /**
-     * Get or set an encrypted cookie's value.
-     *
-     * @param string $name
-     * @param string|null $value
-     * @param int|DateTimeInterface|null $expiry
-     * @param array $options
-     * @return string
-     */
-    public function cookie($name, $value = null, $expiry = null, array $options = [])
-    {
-        if (!is_null($value)) {
-            return $this->addCookie($name, $value, $expiry, $options);
-        }
-        
-        $cookies = array_filter($this->page->cookies(), function ($cookie) use ($name) {
-           return $cookie->name === $name;
-        });
-        
-        if (count($cookies) > 0) {
-            $cookie = $cookies[0];
-        } else {
-            $cookie = null;
-        }
-        
-        if ($cookie) {
-            $decryptedValue = decrypt(rawurldecode($cookie['value']), $unserialize = false);
-            
-            $hasValuePrefix = strpos($decryptedValue, CookieValuePrefix::create($name, Crypt::getKey())) === 0;
-            
-            return $hasValuePrefix ? CookieValuePrefix::remove($decryptedValue) : $decryptedValue;
-        }
-    }
+    // /**
+    //  * Get or set an encrypted cookie's value.
+    //  *
+    //  * @param string $name
+    //  * @param string|null $value
+    //  * @param int|DateTimeInterface|null $expiry
+    //  * @param array $options
+    //  * @return string
+    //  */
+    // public function cookie($name, $value = null, $expiry = null, array $options = [])
+    // {
+    //     if (!is_null($value)) {
+    //         return $this->addCookie($name, $value, $expiry, $options);
+    //     }
+    //
+    //     $cookies = array_filter($this->page->cookies(), function ($cookie) use ($name) {
+    //        return $cookie->name === $name;
+    //     });
+    //
+    //     if (count($cookies) > 0) {
+    //         $cookie = $cookies[0];
+    //     } else {
+    //         $cookie = null;
+    //     }
+    //
+    //     if ($cookie) {
+    //         $decryptedValue = decrypt(rawurldecode($cookie['value']), $unserialize = false);
+    //
+    //         $hasValuePrefix = strpos($decryptedValue, CookieValuePrefix::create($name, Crypt::getKey())) === 0;
+    //
+    //         return $hasValuePrefix ? CookieValuePrefix::remove($decryptedValue) : $decryptedValue;
+    //     }
+    // }
     
     /**
      * Get or set an unencrypted cookie's value.
@@ -52,7 +52,7 @@ trait PuthDuskInteractsWithCookies
     public function plainCookie($name, $value = null, $expiry = null, array $options = [])
     {
         if (!is_null($value)) {
-            return $this->addCookie($name, $value, $expiry, $options, false);
+            return $this->addCookie($name, $value, $expiry, $options);
         }
     
         $cookies = array_filter($this->page->cookies(), function ($cookie) use ($name) {
@@ -77,14 +77,13 @@ trait PuthDuskInteractsWithCookies
      * @param string $value
      * @param int|DateTimeInterface|null $expiry
      * @param array $options
-     * @param bool $encrypt
      * @return $this
      */
-    public function addCookie($name, $value, $expiry = null, array $options = [], $encrypt = true)
+    public function addCookie($name, $value, $expiry = null, array $options = [])
     {
-        if ($encrypt) {
-            $value = encrypt($value, $serialize = false);
-        }
+        // if ($encrypt) {
+        //     $value = encrypt($value, $serialize = false);
+        // }
         
         if ($expiry instanceof DateTimeInterface) {
             $expiry = $expiry->getTimestamp();
